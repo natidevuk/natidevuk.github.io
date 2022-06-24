ca519fe398a3027018e2c27da2628ecff800c291
natidevuk <107931933+natidevuk@users.noreply.github.com>
2022-06-24 23:14:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
 let map;
+const Poland = { lat: 52.237049, lng: 21.017532};
+const Germany = { lat: 52.520008, lng: 13.404954};
+const Czech = { lat: 50.075539, lng: 14.437800};
+let polandMarker, germanyMarker, czechMarker;
+let polandWindow, germanyWindow, czechWindow;
 
 function initMap() {
-    console.log('init')
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 26.357896, lng: 127.783809 },
-        zoom: 3,
+        center: Poland,
+        zoom: 5,
         mapTypeId: 'satellite'
     });
 
-    new google.maps.Marker({
-        position: { lat: 26.357896, lng: 127.783809 },
-        map,
-        title: "Japan",
-      });
+    //Markers
+    polandMarker = placeMarker(Poland, "Poland");
+    germanyMarker = placeMarker(Germany, "Germany");
+    czechMarker = placeMarker(Czech, "Czech");
 
-    new google.maps.Marker({
-        position: {lat: 52.237049, lng: 21.017532},
+    //Windows
+    polandWindow = infoWindow(
+        "Capital: <b> Warsaw </b>" + "<br>" +
+        "Country: <b> Poland 🇵🇱</b>" + "<br>" +
+        "Warsaw Population: <b>1.783m</b> (2020)" + "<br>" +
+        "Poland Population: <b>37.95m</b> (2020)"
+    )
+
+    germanyWindow = infoWindow(
+        "Capital: <b> Berlin </b>" + "<br>" +
+        "Country: <b> Germany 🇩🇪</b>" + "<br>" +
+        "Berlin Population: <b>3.76m</b> (2020)" + "<br>" +
+        "Germany Population: <b>83.24m</b> (2020)"
+    )
+
+    czechWindow = infoWindow(
+        "Capital: <b> Prague </b>" + "<br>" +
+        "Country: <b> Czech 🇨🇿</b>" + "<br>" +
+        "Prague Population: <b>1.3m</b> (2020)" + "<br>" +
+        "Czech Population: <b>10.7m</b> (2020)"
+    )
+
+    //Add listeners
+    giveListener(polandMarker, polandWindow, false);
+    giveListener(germanyMarker, germanyWindow, false);
+    giveListener(czechMarker, czechWindow, false);
+}
+
+function placeMarker(pos, title)
+{
+    return new google.maps.Marker({
+        position: pos,
         map,
-        title: "Poland"
+        title: title
     })
+}
+
+function infoWindow(content)
+{
+    return new google.maps.InfoWindow({
+        content: content,
+    })
+}
+
+function giveListener(marker, window, focus)
+{
+    marker.addListener("click", () => {
+        window.open({
+            anchor: marker,
+            map,
+            shouldFocus: focus,
+        });
+    });
 }
